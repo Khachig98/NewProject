c19e673e1d9e6b1a713bc326737854a419453114
khachig <khachig98@gmail.com>
2019-06-03 23:05:11 +0400

    first commit

---
+++ Internship Task(JS).js
@@ -0,0 +1,16 @@
+var Names=["About","Home","Contacts"];
+
+function About_Alert(){	
+	alert(Names[0] +" Was Clicked");
+	console.log(Names[0] +" Was Clicked");
+}
+
+function Home_Alert(){
+	alert(Names[1]+ " Was Clicked");
+	console.log(Names[1]+ " Was Clicked");
+}
+
+function Contacts_Alert(){
+	alert(Names[2]+ " Was Clicked");
+	console.log(Names[2]+ " Was Clicked");
+}
